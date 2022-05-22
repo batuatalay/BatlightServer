1033cb07f91bbb6c3666a76936c9f55d57a8328f
batuatalay <batureus@gmail.com>
2022-05-23 00:34:39 +0300

    REFACTOR :: color change function moving to contorl.js

--- public/control.js
+++ public/control.js
@@ -247,3 +247,38 @@ function setColor(lampId,colorR,colorG,colorB){
     ws.send("{\"message\":"+JSON.stringify(sendString)+"}");
 
 }
+
+colorWheel1.on('color:change', function(color, changes){
+        device=1;
+        var sendString={
+            deviceid:1,
+            animation:"",
+            R:color.rgb.r,
+            G:color.rgb.g,
+            B:color.rgb.b
+        }
+        console.log(sendString);
+        ws.send("{\"message\":"+JSON.stringify(sendString)+"}");
+    });
+    colorWheel2.on('color:change', function(color, changes){
+        var sendString={
+            deviceid:2,
+            animation:"",
+            R:color.rgb.r,
+            G:color.rgb.g,
+            B:color.rgb.b
+        }
+        console.log(sendString);
+        ws.send("{\"message\":"+JSON.stringify(sendString)+"}");
+    });
+    colorWheel7.on('color:change', function(color, changes){
+        var sendString={
+            deviceid:3,
+            animation:"",
+            R:color.rgb.r,
+            G:color.rgb.g,
+            B:color.rgb.b
+        }
+        console.log(sendString);
+        ws.send("{\"message\":"+JSON.stringify(sendString)+"}");
+    });
