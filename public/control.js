1fabb2ff7ffbbb4f38a059ddff9f642f88fbd527
batuhan-atalay <batuhanatalay@jotform.com>
2022-06-05 15:27:43 +0300

    main.html :: Portable lamp added

--- public/control.js
+++ public/control.js
@@ -213,6 +213,69 @@ $('#scenario3').on('change',function(){
 
     }
 });
+$('#scenario4').on('change',function(){
+       if(this.value=="twoColor"){
+        $('#picker4').html("");
+        $('#picker4').html("<div class='wheel' id='colorWheelDemo8'></div><div class='wheel' id='colorWheelDemo9'></div>");
+        $('#btnDiv4').html("<span class='btn btn-success col-md-12' id='btn1'>Set</span>");
+        var colorWheel11 = new iro.ColorPicker("#colorWheelDemo11");
+        var colorWheel12 = new iro.ColorPicker("#colorWheelDemo12");
+        $('#btn1').on('click',function(){
+            var color1=colorWheel8.color.rgbString;
+            color1=color1.slice(4);
+            color1=color1.slice(0,-1);
+            var color2=colorWheel9.color.rgbString;
+            color2=color2.slice(4);
+            color2=color2.slice(0,-1);
+            var arr1=color2.split(',');
+            var arr2=color1.split(',');
+            var sendString={
+                deviceid:4,
+                animation:"2color",
+                fr:$.trim(arr1[0]),
+                fg:$.trim(arr1[1]),
+                fb:$.trim(arr1[2]),
+                sr:$.trim(arr2[0]),
+                sg:$.trim(arr2[1]),
+                sb:$.trim(arr2[2])
+            };
+
+            ws.send("{\"message\":"+JSON.stringify(sendString)+"}");
+        });
+    } 
+    if(this.value=="rainbow"){
+        var sendString={
+           deviceid:4,
+           animation:"rainbow"
+        }
+        ws.send("{\"message\":"+JSON.stringify(sendString)+"}");
+
+    }
+    if(this.value=="fallin"){
+        var sendString={
+           deviceid:4,
+           animation:"fallin"
+        }
+        ws.send("{\"message\":"+JSON.stringify(sendString)+"}");
+    }
+    if(this.value=="redVibe"){
+        var sendString={
+           deviceid:4,
+           animation:"redVibe"
+        }
+        ws.send("{\"message\":"+JSON.stringify(sendString)+"}");
+
+    }
+    if(this.value=="partyVibe"){
+        var sendString={
+           deviceid:4,
+           animation:"partyVibe"
+        }
+        ws.send("{\"message\":"+JSON.stringify(sendString)+"}");
+
+    }
+});
+
 function party(lampId,fR,fG,fB,sR,sG,sB){
     var sendString={
         deviceid:lampId,
@@ -307,6 +370,13 @@ $('#lamp3').on('click',function () {
     $('#lambader').hide();
     $('#lambader').show();
 });
+$('#lamp4').on('click',function () {
+    $('#client').show();
+    $('#desk').hide();
+    $('#plant').hide();
+    $('#lambader').hide();
+    $('#lambader').show();
+});
 
 ws.onopen = function(e) {
     var sendString = {
@@ -316,6 +386,11 @@ ws.onopen = function(e) {
     ws.send("{\"message\":"+JSON.stringify(sendString)+"}");
     var sendString = {
         deviceid:2,
+        action:"alliveCheck"
+    }
+    ws.send("{\"message\":"+JSON.stringify(sendString)+"}");
+    var sendString = {
+        deviceid:4,
         action:"alliveCheck"
     }
     ws.send("{\"message\":"+JSON.stringify(sendString)+"}");
